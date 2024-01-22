61a555298352c3ee0b1c01160313c54c37bef5f7
Antonyo Andrei <antonyoandrei7@gmail.com>
2024-01-22 12:44:34 +0100

    prisma client import update

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,11 +2,12 @@
 // import { Prisma } from '../../prisma/generated/mongodb_client';
 // import { DefaultArgs } from "@prisma/client/runtime/library";
 
+
 // type ClientMongo = MongoClient<Prisma.PrismaClientOptions, never, DefaultArgs>
 
 // export const mongoClient: ClientMongo = new MongoClient();
 
-import { PrismaClient } from "../../prisma/generated/mongodb_client"
+import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
